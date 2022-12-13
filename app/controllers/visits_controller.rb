b8070112060f9ad4a86edfeff8099b61ecec2ffd
andrew <andrewtehanov@gmail.com>
2022-12-13 19:41:23 +0300

    End visit controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,45 +1,25 @@
 class VisitsController < ApplicationController 
+  before_action :find_medical_center, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  before_action :find_department, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  before_action :find_doctor, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  before_action :find_visit, only: [:show, :edit, :update, :destroy]
+
   def index
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
     @visits = @doctor.visits
   end
 
-  def new
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-  end
+  def new; end
 
   def create
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-    binding.pry
     @visit = @doctor.visits.create(visit_params)
-    redirect_to medical_center_department_doctor_visits_path(@medical_center, @department, @doctor)
+    redirect_to medical_center_department_doctor_visit_path(@medical_center, @department, @doctor, @visit)
   end
 
-  def show
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-    @visit = @doctor.visits.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-    @visit = @doctor.visits.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-    @visit = @doctor.visits.find(params[:id])
     if @visit.update(visit_params)
       redirect_to medical_center_department_doctor_visit_path(@medical_center, @department, @doctor, @visit)
     else
@@ -48,15 +28,27 @@ class VisitsController < ApplicationController
   end
 
   def destroy
-    @medical_center = MedicalCenter.find(params[:medical_center_id])
-    @department = @medical_center.departments.find(params[:department_id])
-    @doctor = @department.doctors.find(params[:doctor_id])
-    @visit = @doctor.visits.find(params[:id])
     @visit.destroy
     redirect_to medical_center_department_doctor_visits_path(@medical_center, @department, @doctor)
   end
 
   private
+
+  def find_medical_center
+    @medical_center = MedicalCenter.find(params[:medical_center_id])
+  end
+
+  def find_department
+    @department = @medical_center.departments.find(params[:department_id])
+  end
+
+  def find_doctor
+    @doctor = @department.doctors.find(params[:doctor_id])
+  end
+
+  def find_visit
+    @visit = @doctor.visits.find(params[:id])
+  end
 
   def visit_params
     params.require(:visit).permit(:datetime, :doctor_id, :patient_id)
